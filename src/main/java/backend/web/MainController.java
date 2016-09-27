fd97392849083c9fbbaa85079bb8bd0d5a8c8e67
Viacheslav Inozemtsev <viacheslav.inozemtsev@zalando.de>
2016-09-27 21:55:43 +0200

    Commit to test jenkins 2

--- src/main/java/backend/web/MainController.java
+++ src/main/java/backend/web/MainController.java
@@ -25,7 +25,7 @@ public class MainController {
     @ResponseStatus(HttpStatus.OK)
     public String index() {
         LOGGER.info("index()");
-        return "Greetings from ReadLearn backend service! Hahaha";
+        return "Greetings from ReadLearn backend service!";
     }
 
     @RequestMapping(value = "/lookup/{word}", method = RequestMethod.GET)
